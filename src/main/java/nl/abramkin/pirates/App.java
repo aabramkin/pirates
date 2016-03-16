6d86e93632b2264d48258218f59e2bf495dd1116
aabramkin <alexei@abramkin.nl>
2016-03-16 12:56:11 +0300

    Revert "Added DI instead of Factory Methods"
    This reverts commit 72636fac783d2dc8c69a7ef997b716afc1bf65a8.

--- src/main/java/nl/abramkin/pirates/App.java
+++ src/main/java/nl/abramkin/pirates/App.java
@@ -1,22 +1,18 @@
 package nl.abramkin.pirates;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 /**
  *
  */
-public class App {
-    public static void main(String[] args) {
-        if (args.length != 2) {
+public class App 
+{
+    public static void main( String[] args )
+    {
+        if(args.length != 2) {
             throw new RuntimeException(String.format("Wrong number of arguments %1$s", args.length));
         }
         String path = args[0];
         int numOfGallons = Integer.parseInt(args[1]);
-
-        ApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"beans.xml"});
-
-        JackSparrowHelper helper = context.getBean("fairlyHelper", JackSparrowHelper.class);
+        JackSparrowHelper helper = JackSparrowHelperImpl.getInstance();
         helper.helpJackSparrow(path, numOfGallons);
     }
 }

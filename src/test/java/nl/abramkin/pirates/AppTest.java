07aafe59502b42871c6ade38684ee5cb5d981f3d
aabramkin <alexei@abramkin.nl>
2016-03-16 13:19:52 +0300

    Added DI instead of Factory Methods

--- src/test/java/nl/abramkin/pirates/AppTest.java
+++ src/test/java/nl/abramkin/pirates/AppTest.java
@@ -1,55 +1,55 @@
 package nl.abramkin.pirates;
 
 import junit.framework.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "file:src/main/java/beans.xml")
+public class AppTest extends TestCase {
+
+    @Autowired
+    private JackSparrowHelperImpl fairlyHelper;
+
+    public AppTest() {
+        super("Test");
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
     }
 
     /**
      * Test App
      */
-    public void testApp()
-    {
+    @org.junit.Test
+    public void testApp() {
         final String path = "sources.csv";
-        JackSparrowHelper helper = JackSparrowHelperImpl.getInstance();
 
-        Purchases purchases = helper.helpJackSparrow(path, 555);
+        Purchases purchases = fairlyHelper.helpJackSparrow(path, 555);
         assertTrue(purchases.calculateAveragePrice().intValue() == 51);
 
-        purchases = helper.helpJackSparrow(path, 500);
+        purchases = fairlyHelper.helpJackSparrow(path, 500);
         assertTrue(purchases.calculateAveragePrice() == 50.7);
 
-        purchases = helper.helpJackSparrow(path, 10);
+        purchases = fairlyHelper.helpJackSparrow(path, 10);
         assertTrue(purchases.calculateAveragePrice().intValue() == 50);
 
-        purchases = helper.helpJackSparrow(path, 0);
+        purchases = fairlyHelper.helpJackSparrow(path, 0);
         assertTrue(purchases.getPurchases().isEmpty());
 
-        purchases = helper.helpJackSparrow(path, 1000);
+        purchases = fairlyHelper.helpJackSparrow(path, 1000);
         assertNull(purchases);
 
-        purchases = helper.helpJackSparrow(path, 200);
+        purchases = fairlyHelper.helpJackSparrow(path, 200);
         assertTrue(purchases.calculateAveragePrice() == 50.5);
     }
 }
